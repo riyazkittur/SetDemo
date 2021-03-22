a2217a54b8de37d048ab7f4c803484ea9bc94f1e
Riyazahmed K <riyazahmed.k@in.abb.com>
2021-03-22 23:14:03 +0530

    Added IsEmpty and Count methods in Set

--- SetData.Tests/SetDataTest.cs
+++ SetData.Tests/SetDataTest.cs
@@ -5,10 +5,42 @@ namespace SetData.Tests
 {
     public class SetDataTest
     {
+        /**unique elements*/
+        //Order of elements is of no significance
         [Fact]
-        public void Test1()
+        public void IsEmptySetTest()
         {
+            SetData empty = new SetData();
+            Assert.True(empty.IsEmpty());
 
+            SetData one = new SetData();
+            one.Add("1");
+            Assert.False(one.IsEmpty());
+
+            SetData three = new SetData();
+            three.Add("1");
+            three.Add("2");
+            three.Add("3");
+
+            Assert.False(three.IsEmpty());
         }
+        [Fact]
+        public void CountTest()
+        {
+            SetData empty = new SetData();
+            Assert.Equal(0,empty.Count());
+
+            SetData one = new SetData();
+            one.Add("1");
+            Assert.Equal(1, one.Count());
+
+            SetData three = new SetData();
+            three.Add("1");
+            three.Add("2");
+            three.Add("3");
+            Assert.Equal(3, three.Count());
+        }
+        
+
     }
 }
--- SetData/SetData.cs
+++ SetData/SetData.cs
@@ -4,5 +4,20 @@ namespace SetData
 {
     public class SetData
     {
+        public int ElementsCount = 0;
+        public bool IsEmpty()
+        {
+            return ElementsCount==0;
+        }
+        public void Add(object element)
+        {
+            ElementsCount += 1;
+            
+        }
+        public int Count()
+        {
+            return ElementsCount;
+        }
+      
     }
 }
